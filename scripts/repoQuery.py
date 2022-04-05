34a44639383bda9b28cb587a7811b322d36080b2
Ian Guelman <ianguelman@gmail.com>
2022-04-05 15:59:09 -0300

    Feat: Query para repositórios populares + Persistencia
    Criação da query para busca de repositórios e persistencia destes no mongo

---
+++ scripts/repoQuery.py
@@ -0,0 +1,84 @@
+from math import ceil
+import os
+from types import NoneType
+from utils.graphql import GraphQL
+from utils.mongo import Mongo
+
+TOTAL_ITEMS = 100
+PER_PAGE = 25
+
+def run():
+    items_count = Mongo().get_documents_count("repo")
+
+    if items_count < TOTAL_ITEMS:
+
+        graphql = GraphQL(os.environ["API_URL"])
+        last_cursor = None
+
+        while items_count < TOTAL_ITEMS:
+            response = graphql.post(
+                """
+                query popularRepositories($lastCursor: String, $perPage: Int) {
+                    search(
+                        query: "stars:>100"
+                        type: REPOSITORY
+                        after: $lastCursor
+                        first: $perPage
+                    ) {
+                        nodes {
+                        ... on Repository {
+                            url
+                            name
+                            owner {
+                                login
+                            }
+                            stargazerCount
+                            mergedPr: pullRequests(first: 1, states: MERGED) {
+                                totalCount
+                            }
+                            closedPr: pullRequests(first: 1, states: CLOSED) {
+                                totalCount
+                            }
+                        }
+                        }
+                        pageInfo {
+                        endCursor
+                        hasNextPage
+                        }
+                    }
+                    }
+                """,
+                {
+                    "lastCursor": last_cursor,
+                    "perPage": PER_PAGE,
+                },
+            )
+            
+            last_cursor = response["data"]["search"]["pageInfo"]["endCursor"]
+                        
+            formatter = lambda node : {
+                "url" : node["url"],
+                "name": node["name"],
+                "owner": node["owner"]["login"],
+                "stargazerCount": node["stargazerCount"],
+                "mergedPr": node["mergedPr"]["totalCount"],
+                "closedPr": node["closedPr"]["totalCount"],
+                "processed": False
+            }
+
+            for node in list(map(formatter, response["data"]["search"]["nodes"])):
+                if int(node["mergedPr"]) + int(node["closedPr"]) >= 100:
+                    Mongo().insert_one(node, "repo")
+
+                    items_count = Mongo().get_documents_count("repo")
+                    print(f'{items_count} nodes of {TOTAL_ITEMS}')
+
+                    if items_count >= TOTAL_ITEMS:
+                        break
+
+            if not response["data"]["search"]["pageInfo"]["hasNextPage"]:
+                break
+
+
+    else:
+        print(f"DB already populated with {items_count} repos")
---
+++ utils/mongo.py
@@ -0,0 +1,50 @@
+from ast import List
+import os
+from pydoc import doc
+from typing import Collection
+import pymongo
+
+class Mongo:
+    
+    __client: any
+    __database: any
+    __collection: any
+    
+    def __init__(self):
+        self.__client = pymongo.MongoClient(
+            host=os.environ['DATABASE_HOST'],
+            port=int(os.environ['DATABASE_PORT']),
+            username=os.environ['DATABASE_USERNAME'],
+            password=os.environ['DATABASE_PASSWORD'],
+        )
+        self.__database = self.__client[os.environ['PRIMARY_DATABASE']]
+
+
+    def set_collection(self, collection):
+        if collection == 'repo':
+           self.__collection = self.__database[os.environ['REPO_COLLECTION']]
+        elif collection == 'pr':
+           self.__collection = self.__database[os.environ['PR_COLLECTION']]  
+        
+    def insert_one(self, value, collection):
+        self.set_collection(collection)
+        self.__collection.insert_one(value)
+        
+    def insert_many(self, value, collection):
+        self.set_collection(collection)
+        self.__collection.insert_many(value)
+
+    def update_one(self, query, value, collection):
+        self.set_collection(collection)
+        self.__collection.update_one(query, value)
+
+    def get_documents_count(self, collection):
+        self.set_collection(collection)
+        return self.__collection.count_documents({})
+    
+    def get_all_documents(self, collection):
+        self.set_collection(collection)
+        documents = []
+        for document in self.__collection.find({}):
+          documents.append(document)
+        return documents
