0ae7e29bd08c88642373fc3cbd9d5803f523e97b
Ian Guelman <ianguelman@gmail.com>
2022-04-06 17:52:23 -0300

    Feat: Logs melhorados e finalização da query
    Finalização da query e melhoria nos logs para avaliar seu processo

--- scripts/prQuery.py
+++ scripts/prQuery.py
@@ -1,5 +1,6 @@
 from math import ceil
 import os
+import sys
 from types import NoneType
 from dateutil.parser import parse
 from utils.graphql import GraphQL
@@ -7,11 +8,12 @@ from utils.mongo import Mongo
 
 PER_PAGE = 50
 
+
 def run():
     repos_count = Mongo().get_documents_count("repo")
     processed_count = Mongo().get_processed_documents_count("repo")
 
-    repos = [Mongo().get_all_documents("repo")[0]]
+    repos = Mongo().get_all_documents("repo")
 
     if processed_count < repos_count:
 
@@ -21,9 +23,10 @@ def run():
         for repo in repos:
             if repo["processed"] is False:
 
-                query_ended = False
-                
+                prAnalysed = 0
+
                 while not query_ended:
+                    
                     response = graphql.post(
                         """
                         query pullRequests($name: String!, $owner: String!, $perPage: Int, $lastCursor: String) {
@@ -66,10 +69,13 @@ def run():
                             "perPage": PER_PAGE,
                         },
                     )
-                    
+
+                    query_ended = True
+
                     last_cursor = response["data"]["repository"]["pullRequests"]["pageInfo"]["endCursor"]
-                                
-                    formatter = lambda node : {
+                    prCount = response["data"]["repository"]["pullRequests"]["totalCount"]
+
+                    def formatter(node): return {
                         "reviews": node["reviews"]["totalCount"],
                         "merged": node["merged"],
                         "closed": node["closed"],
@@ -77,7 +83,7 @@ def run():
                         "mergedAt": node["mergedAt"],
                         "mergeTime": (parse(node["mergedAt"]).replace(tzinfo=None) - parse(node["createdAt"]).replace(tzinfo=None)).seconds * 60 if node["merged"] else None,
                         "closedAt": node["closedAt"],
-                        "closeTime": (parse(node["closedAt"]).replace(tzinfo=None) - parse(node["createdAt"]).replace(tzinfo=None)).seconds * 60 if node["closed"] else None, 
+                        "closeTime": (parse(node["closedAt"]).replace(tzinfo=None) - parse(node["createdAt"]).replace(tzinfo=None)).seconds * 60 if node["closed"] else None,
                         "files": node["files"]["totalCount"],
                         "bodySize": len(node["bodyText"]),
                         "changedFiles": node["changedFiles"],
@@ -85,22 +91,28 @@ def run():
                         "comments": node["comments"],
                     }
 
-                    nodes = list(map(formatter, response["data"]["repository"]["pullRequests"]["nodes"]))
+                    nodes = list(
+                        map(formatter, response["data"]["repository"]["pullRequests"]["nodes"]))
 
                     for node in nodes:
+                        prAnalysed += 1
                         if int(node["reviews"]) >= 1 and (node["closed"] or node["merged"]):
-                            if min(node["mergeTime"], node["closeTime"]) >= 60:
+                            if min(node["mergeTime"] if node["merged"] else sys.maxsize, node["closeTime"] if node["closed"] else sys.maxsize,) >= 60:
                                 Mongo().insert_one(node, "pr")
 
-                                print(f'{repo["name"]} - Pull request data added to DB')
-
+                                print(
+                                    f'{repo["name"]} - Pull request data added to DB - {prAnalysed} pull requests analysed out of {prCount} ({prCount - prAnalysed} left)')
 
                     if not response["data"]["repository"]["pullRequests"]["pageInfo"]["hasNextPage"]:
-                        print(f'{repo["name"]} - All pull requests analysed')
-                        Mongo().update_one({'url': repo["url"]}, {'$set' : { 'processed': True } })
+                        reposLen = repos.len()
+                        repoIndex = repos.index(repo)
+                        print(f'{repo["name"]} - All pull requests analysed - {repoIndex} repositories analysed out of {reposLen} ({reposLen - repoIndex} left)')
+                        Mongo().update_one({'url': repo["url"]}, {
+                            '$set': {'processed': True}})
                         query_ended = True
                         break
             else:
-                print(f'{repo["name"]} - Repositories had already been processed')
+                print(
+                    f'{repo["name"]} - Repository had already been processed')
     else:
         print(f'All {repos_count} repositories had already been processed')
