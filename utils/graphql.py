4a106067f0291755da4792cce60c57acd0564e25
mateus-coder1 <letsgood56@gmail.com>
2022-04-18 16:04:21 -0300

    Refactor: Modificada lógica de alteração do token

--- utils/graphql.py
+++ utils/graphql.py
@@ -1,18 +1,21 @@
 import os
 import requests
 import json
+import time
 
 class GraphQL:
     
     api: str
     tokens = []
+    retries: int
     
     def __init__(self, api):
         self.api = api
         self.tokens = os.environ.get('TOKENS').split(',')
+        self.retries = 0
         
-    def post(self, query, variables, retries):
-        token = self.tokens[retries % len(self.tokens)]
+    def post(self, query, variables):
+        token = self.tokens[self.retries]
         request = requests.post(
             self.api, json={'query': query, "variables": variables}, 
             headers={'Authorization': f'bearer {token}'}
@@ -20,11 +23,11 @@ class GraphQL:
         if request.status_code == 200:
             return json.loads(request.text)
         elif request.status_code == 502:
-            token
-            retries += 1
-            self.post(query, variables, retries)
+            self.retries = self.retries + 1 if self.retries + 1 < len(self.tokens) else 0 
+            self.post(query, variables)
         else:
             raise Exception(f'Query failed, with status code {request.status_code}')
+        time.sleep(5)
 
 
         
