175c1d9480ac19253515b66187914ca6d7571576
Ian Guelman <ianguelman@gmail.com>
2022-04-19 16:04:36 -0300

    Feat: tratamento de exceções na query
    Co-authored-by: Arthur Gregorio <arthurgregorioleal@gmail.com>

--- utils/graphql.py
+++ utils/graphql.py
@@ -1,3 +1,4 @@
+import math
 import os
 import requests
 import json
@@ -16,14 +17,25 @@ class GraphQL:
         
     def post(self, query, variables):
         token = self.tokens[self.retries]
-        request = requests.post(
-            self.api, json={'query': query, "variables": variables}, 
-            headers={'Authorization': f'bearer {token}'},
-            stream=True
-        )
-        time.sleep(0.1)
+        try:
+            request = requests.post(
+                self.api, json={'query': query, "variables": variables}, 
+                headers={'Authorization': f'bearer {token}'},
+                stream=True
+            )
+            time.sleep(0.1)
+        except Exception as e:
+            print(str(e))
+            print("Sleeping...")
+            time.sleep(60 * 5)
         if request.status_code == 200:
-            return json.loads(request.text)
+            try:
+                return json.loads(request.text)
+            except Exception as e:
+                print(str(e))
+                print("Reducing perPage size...")
+                variables["perPage"] = math.floor(variables["perPage"]/2)
+                self.post(query, variables)
         elif request.status_code == 502:
             self.retries = self.retries + 1 if self.retries + 1 < len(self.tokens) else 0 
             print("Retrying query...")
